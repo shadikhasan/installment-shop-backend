82c209de22496a2d979d9cf6ea2276a85173abfc
EKRAMUL ISLAM SHADIK <48581997+shadikhasan@users.noreply.github.com>
2025-05-15 14:30:22 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -156,8 +156,8 @@ SIMPLE_JWT = {
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'shadik.sk420@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'yktqaaumcxxbqkra')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'your email')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'your password')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 
 # Celery settings
